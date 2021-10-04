5022b2bb01e3c43825d741ce325cc4bc35ffeb29
Seefane <seefanephasha33@gmail.com>
2021-10-04 10:24:57 +0200

    django heroku

--- librarymanager/settings.py
+++ librarymanager/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+import django_heroku
 import environ
 
-env = environ.Env()
-environ.Env.read_env()
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -162,6 +163,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+
+django_heroku.settings(locals())
