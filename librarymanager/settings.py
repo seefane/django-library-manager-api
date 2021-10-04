10b67d2cab8bb372164f3cec34bf08bf5adabd8c
Seefane <seefanephasha33@gmail.com>
2021-10-05 00:44:55 +0200

    key

--- librarymanager/settings.py
+++ librarymanager/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 import dj_database_url
 import environ
-import django_heroku
+
 
 
 env = environ.Env()
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["restlibrarymanager.herokuapp.com"]
 
